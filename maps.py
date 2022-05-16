349427a1921aae69a059688e0b4b326e36a6084f
Asyps <105502574+Asyps@users.noreply.github.com>
2022-05-16 13:10:38 +0200

    a new map format
    might undergo further changes

--- maps.py
+++ maps.py
@@ -3,3 +3,12 @@ map_test = [[["blank","wall"], ["blank","wall"], ["blank", "wall"], ["blank", "w
     [["blank", "wall"], ["blank", "blank"], ["blank", "blank"], ["blank", "wall"]],
     [["blank", "wall"], ["blank", "blank"], ["blank", "blank"], ["blank", "wall"]],
     [["blank", "wall"], ["blank", "wall"], ["blank", "wall"], ["blank", "wall"]]]
+
+map_test = {
+    "size": (4, 5);
+    (0, 0): "wall"; (0, 1): "wall"; (0, 2): "wall"; (0, 3): "wall"; 
+    (1, 0): "wall";                                 (1, 3): "wall";
+    (2, 0): "wall"; (2, 1): "player";               (2, 3): "wall";
+    (3, 0): "wall";                                 (3, 3): "wall";
+    (4, 0): "wall"; (4, 1): "wall"; (4, 2): "wall"; (4, 3): "wall"; 
+}
