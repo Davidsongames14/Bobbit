658c081036ce53a0cd533452637b28bb49ad11cd
Asyps <105502574+Asyps@users.noreply.github.com>
2022-05-16 14:44:04 +0200

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,14 +1,14 @@
 graphics_strings = {
-    "wall": "\033[100m  ",                  #gray
-    "player": "\033[44m  ",                 #blue
-    "friend": "\033[42m  ",                 #green
-    "enemy": "\033[41m  ",                  #red
-    "collectable": "\033[103m  ",           #yellow
-    "door": "\033[107m  ",                  #white (placeholder, but might stay if no better color is chosen)
-    "teleport": "\033[0m  "                 #no color
+    "wall": "\033[100m  ",                  # gray
+    "player": "\033[44m  ",                 # blue
+    "friend": "\033[42m  ",                 # green
+    "enemy": "\033[41m  ",                  # red
+    "collectable": "\033[103m  ",           # yellow
+    "door": "\033[107m  ",                  # white (placeholder, but might stay if no better color is chosen)
+    "teleport": "\033[0m  "                 # no color
 }
 
-def render_map(map):           #renders the map
+def render_map(map):           # renders the map
     map_size = map["size"]
     for y in range(map_size[1]):
         for x in range(map_size[0]):
