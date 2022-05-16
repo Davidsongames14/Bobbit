5d6e07b51134d6f3343d16c27b630f8d3ba651b2
Asyps <105502574+Asyps@users.noreply.github.com>
2022-05-16 13:57:11 +0200

    a new map format
    removes tons of junk

--- graphics.py
+++ graphics.py
@@ -10,10 +10,15 @@ graphics_strings = {
 }
 
 def render_map(map):           #renders the map
-    for i in range(map["size"][0]):
-        for j in range(map["size"][1]:
-            if (i, j) in map.keys():
-                print(graphics_strings[map[(i, j)]], end="")
+    map_size = map["size"]
+    for y in range(map_size[1]):
+        for x in range(map_size[0]):
+            coords = (x, y)
+            if coords in map.keys():
+                print(graphics_strings[map[coords]], end="")
             else:
-                print("\033[0m  ", end="")
+                if 0 in coords or coords[0] == map_size[0]-1 or coords[1] == map_size[1]-1:
+                    print(graphics_strings["wall"], end="")
+                else:
+                    print("\033[0m  ", end="")
         print("\033[0m")
--- maps.py
+++ maps.py
@@ -1,14 +1,6 @@
-map_test = [[["blank","wall"], ["blank","wall"], ["blank", "wall"], ["blank", "wall"]],             
-    [["blank", "wall"], ["player", "blank"], ["blank", "blank"], ["blank", "wall"]], 
-    [["blank", "wall"], ["blank", "blank"], ["blank", "blank"], ["blank", "wall"]],
-    [["blank", "wall"], ["blank", "blank"], ["blank", "blank"], ["blank", "wall"]],
-    [["blank", "wall"], ["blank", "wall"], ["blank", "wall"], ["blank", "wall"]]]
+#size: (width, height)
 
 map_test = {
     "size": (4, 5),
-    (0, 0): "wall", (0, 1): "wall", (0, 2): "wall", (0, 3): "wall", 
-    (1, 0): "wall",                                 (1, 3): "wall",
-    (2, 0): "wall", (2, 1): "player",               (2, 3): "wall",
-    (3, 0): "wall",                                 (3, 3): "wall",
-    (4, 0): "wall", (4, 1): "wall", (4, 2): "wall", (4, 3): "wall", 
+    (1, 2): "player"
 }
